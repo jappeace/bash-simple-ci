74cf800ca9af9ea77393caf4ba880ea2433ae523
Jappie Klooster <jappieklooster@hotmail.com>
2013-11-28 13:57:02 +0100

    now the remote file is first removed and the cache is cleared

--- build.sh
+++ build.sh
@@ -3,15 +3,29 @@
 
 projectPath="$HOME/berg"
 localfile="$HOME/grails.war"
-remotefile="/var/lib/tomcat7/webapps/ROOT.war"
+tomcat="/var/lib/tomcat7"
+remotefile="$tomcat/webapps/ROOT.war"
+cache="$tomcat/work/Catalina"
 server=root@85.17.248.15
 
 cd $projectPath
 echo "building file"
 grails dev war $localfile
+
+echo "stopping server"
+ssh $server 'service tomcat7 stop'
+
+echo "removing old war file"
+ssh $server "rm $remotefile"
+
+echo "removing cache"
+ssh $server "rm -R $cache/*"
+
 echo "deploying file"
 scp $localfile $server:$remotefile
+
 echo "restarting server"
-ssh $server 'service tomcat7 restart'
+ssh $server 'service tomcat7 start'
+
 echo "removing local build result"
 rm $localfile
