e39f513ae72684f712bd00d6b2f882959fb58b98
Jappie Klooster <jappieklooster@hotmail.com>
2013-11-28 10:10:04 +0100

    moved script to bin for easy adding to path

---
+++ bin/build.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+projectPath="$HOME/berg"
+localfile="$HOME/grails.war"
+remotefile="/var/lib/tomcat7/webapps/ROOT.war"
+server=root@85.17.248.15
+
+cd $projectPath
+echo "building file"
+grails dev war $localfile
+echo "deploying file"
+scp $localfile $server:$remotefile
+echo "restarting server"
+ssh $server 'service tomcat7 restart'
